3968f2df213fac97bdba7e213abcf5329b453e17
Nitinai Srikhao <nitinai.sr@gmail.com>
2023-07-06 14:57:10 +0700

    update checkLoginUser

--- src/app/utils.tsx
+++ src/app/utils.tsx
@@ -6,6 +6,8 @@ export function checkLoginStatus() {
 
      // eslint-disable-next-line react-hooks/rules-of-hooks
      const [router,setRouter] =useState(useRouter());
+          // eslint-disable-next-line react-hooks/rules-of-hooks
+     const [user,setUser] = useState<any>('');
 
  
 
@@ -16,10 +18,10 @@ export function checkLoginStatus() {
         const oneDayInMilliseconds = 24 * 60 * 60 * 1000; // 1 day in milliseconds
       
         if (lastLoginTime && currentTime - Number.parseInt(lastLoginTime) < oneDayInMilliseconds) {
-          router.push('/main');
+          setUser(localStorage.getItem('recentUsername'));
         } else {
 
-          router.replace('/login');
+          setUser('');
         }
     },[]);
 
