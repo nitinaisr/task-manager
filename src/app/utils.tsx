94734133ef6363c04acb385e764a3ad2805284d4
Nitinai Srikhao <nitinai.sr@gmail.com>
2023-07-06 14:51:35 +0700

    update checkLogInStatus function

--- src/app/utils.tsx
+++ src/app/utils.tsx
@@ -7,23 +7,6 @@ export function checkLoginStatus() {
      // eslint-disable-next-line react-hooks/rules-of-hooks
      const [router,setRouter] =useState(useRouter());
 
-
-    //  const lastLoginTime = localStorage.getItem('lastLoginTime');
-    //  const currentTime = new Date().getTime();
-    //  const oneDayInMilliseconds = 24 * 60 * 60 * 1000; // 1 day in milliseconds
-   
-    //  if (lastLoginTime && currentTime - Number.parseInt(lastLoginTime) < oneDayInMilliseconds) {
-    //    router.push('/main');
-    //  } else {
-   
-    //    console.debug('else');
-    // //    // User needs to perform a fresh login, update the last login time and perform necessary actions
-    // //    localStorage.setItem('lastLoginTime', currentTime.toString());
-    //    router.replace('/login');
-    //  }
-
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    // setRouter(useRouter());
  
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -35,14 +18,8 @@ export function checkLoginStatus() {
         if (lastLoginTime && currentTime - Number.parseInt(lastLoginTime) < oneDayInMilliseconds) {
           router.push('/main');
         } else {
-      
-          console.debug('else');
-       //    // User needs to perform a fresh login, update the last login time and perform necessary actions
-       //    localStorage.setItem('lastLoginTime', currentTime.toString());
-       setTimeout(() => {
-        router.replace('/login');
-       }, 5000);
-        //   router.replace('/login');
+
+          router.replace('/login');
         }
     },[]);
 
