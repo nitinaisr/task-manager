c5931b55bffb6d04614d2a88897ae0c451983909
Nitinai Srikhao <nitinai.sr@gmail.com>
2023-07-06 16:01:11 +0700

    update checkLoginStatus function

--- src/app/utils.tsx
+++ src/app/utils.tsx
@@ -1,28 +1,21 @@
 'use client';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
 
 export function checkLoginStatus() {
 
-     // eslint-disable-next-line react-hooks/rules-of-hooks
-     const [router,setRouter] =useState(useRouter());
-          // eslint-disable-next-line react-hooks/rules-of-hooks
-     const [user,setUser] = useState<any>('');
-
- 
-
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
+
+    if (typeof window !== 'undefined') {
+
         const lastLoginTime = localStorage.getItem('lastLoginTime');
         const currentTime = new Date().getTime();
         const oneDayInMilliseconds = 24 * 60 * 60 * 1000; // 1 day in milliseconds
+        let isExpired = true;
       
         if (lastLoginTime && currentTime - Number.parseInt(lastLoginTime) < oneDayInMilliseconds) {
-          setUser(localStorage.getItem('recentUsername'));
-        } else {
-
-          setUser('');
+          isExpired = false;
         }
-    },[]);
 
+        return isExpired;
+
+      }
 }
