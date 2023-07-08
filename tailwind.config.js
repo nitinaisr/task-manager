2f5f303cd9d6bbc6de59c2d370ee504319c91496
Nitinai Srikhao <nitinai.sr@gmail.com>
2023-07-08 21:05:41 +0700

    update tailwind config and add css class global

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+        colors:{
+            "whitesmoke": "#f8f8f8",
+            "lavender": {
+                50: '#A1BBF5',
+                500: '#8B9EEF',
+            }
+        },
+        "borderRadius": {
+            "20xl": "20px"},
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
