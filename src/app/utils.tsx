4d988d9afee51c703d21b7980e77802769df2128
Nitinai Srikhao <nitinai.sr@gmail.com>
2023-07-08 21:07:26 +0700

    update add function get use name, add function log out, add function format date

--- src/app/utils.tsx
+++ src/app/utils.tsx
@@ -1,21 +1,52 @@
-'use client';
+"use client";
 
 export function checkLoginStatus() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
+  if (typeof window !== "undefined") {
+    const lastLoginTime = localStorage.getItem("lastLoginTime");
+    const currentTime = new Date().getTime();
+    const oneDayInMilliseconds = 24 * 60 * 60 * 1000; // 1 day in milliseconds
+    let isExpired = true;
 
-    if (typeof window !== 'undefined') {
+    if (
+      lastLoginTime &&
+      currentTime - Number.parseInt(lastLoginTime) < oneDayInMilliseconds
+    ) {
+      isExpired = false;
+    }
 
-        const lastLoginTime = localStorage.getItem('lastLoginTime');
-        const currentTime = new Date().getTime();
-        const oneDayInMilliseconds = 24 * 60 * 60 * 1000; // 1 day in milliseconds
-        let isExpired = true;
-      
-        if (lastLoginTime && currentTime - Number.parseInt(lastLoginTime) < oneDayInMilliseconds) {
-          isExpired = false;
-        }
+    return isExpired;
+  }
+}
 
-        return isExpired;
+export  function  getUserName():string {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
 
-      }
+  if (typeof window !== "undefined") {
+    const userName =localStorage.getItem("recentUsername");
+    return userName ? userName:'';
+  }
+  return "";
+}
+
+export  function  logOut() {
+  if (typeof window !== "undefined") {
+    localStorage.setItem('lastLoginTime', '');
+  }
+}
+
+export function formatDate(date: string | number | Date) {
+  const options: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  };
+  const formattedDate = new Date(date).toLocaleDateString("en-UK", options);
+
+  const [day, month, year] = formattedDate.split(" ");
+
+  const capitalizedMonth = month.toUpperCase();
+
+  return `${day} ${capitalizedMonth} ${year}`;
 }
