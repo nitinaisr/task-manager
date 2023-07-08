39b090e8d4fee93fa872e3d05112d8b0319c8137
Nitinai Srikhao <nitinai.sr@gmail.com>
2023-07-08 21:10:04 +0700

    update page and layout for responsive

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,13 +1,55 @@
-import React from "react";
+'use client';
+import React, { use, useEffect, useState } from "react";
+import { getUserName, logOut } from "../utils";
+import Image from "next/image";
 
 type Props = {
   children: React.ReactNode;
 };
 
 function layout({ children }: Props) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [userName,setUserName] = useState('');
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [isActive, setIsActive] = useState(false);
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        setUserName(getUserName());
+    },[userName]);
+
+    const handleClick = (event: any) => {
+        setIsActive(current => !current);
+      };
   return (
     <div>
-      <h1>tab menu</h1>
+
+
+<div  className='absolute z-10 top-0 right-0 cursor-pointer bottom-0 m-5 w-14 h-14 text-white transition-colors duration-325 bg-whitesmoke rounded-full' >
+             <Image
+            src={'/account.svg'}
+            width={60}
+            height={60}
+            alt={""}
+            className="rounded-lg"
+            onClick={handleClick}
+          />
+                                  <ul className={isActive ? 'dropdown active hover:bg-lavender-500':'dropdown'}>
+      <li><a href="/login" onClick={logOut}>Logout</a></li>
+    </ul>
+</div>
+
+
+
+             
+      <div className="sm:p-10  md:p-20 xl:p-30 p-5 flex flex-col justify-center relative rounded-t-none rounded-b-20xl bg-lavender-50 w-full h-[15.63rem]">
+      <h1>Hi! {userName}</h1>
+        <h4>This is just a sample UI. <br/>
+        Open to create your style :D
+        </h4>
+      </div>
+      
+
       <div>{children}</div>
     </div>
   );
--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,15 +1,36 @@
-'use client'
-import React from 'react'
-import { checkLoginStatus } from '@/app/utils';
+"use client";
+import React, {useState, useEffect } from "react";
+import TaskList from "./components/task-list";
+import { checkLoginStatus } from "../utils";
+import { useRouter } from 'next/navigation';
 
-type Props = {}
+type Props = {};
 
 const main = () => {
-    checkLoginStatus();
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [isExpired, setIsExpired] = useState<any>(checkLoginStatus());
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [router, setRouter] = useState(useRouter());
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+
+
+    if(isExpired){
+        router.replace('/login');
+
+    }else{
+      return;
+    }
+
+  });
 
   return (
-    <div>main</div>
-  )
-}
+    <div className="sm:m-10 sm:mt-0 md:m-20 md:mt-0 xl:m-30 xl:mt-0  m-5 mt-0">
+      <TaskList />
+    </div>
+  );
+};
 
 export default main;
